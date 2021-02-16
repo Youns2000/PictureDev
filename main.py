c5960301dd6250788d4d7dbcc9f7e3c97c2be703
Youns2000 <56587811+Youns2000@users.noreply.github.com>
2021-02-16 19:15:08 +0100

    config file

--- main.py
+++ main.py
@@ -8,14 +8,33 @@ from datetime import datetime as dt
 class call:
     def __init__(self):
         self.last = 0
+        self.norm = 0
+        self.key = 0
+        self.t = 0
+
     def print_sound(self,indata, outdata, frames, times, status):
-        print(self.last)
         volume_norm = np.linalg.norm(indata)*10
-        if(int(volume_norm)>220 and (int(dt.now().strftime('%S'))-self.last)>1): 
+        if(int(volume_norm)>self.norm and (float(dt.now().strftime('%S.%f'))-float(self.last))>float(self.t)): 
             self.last = int(dt.now().strftime('%S'))
-            keyboard.press_and_release('space')
+            keyboard.press_and_release(self.key)
 
 if __name__ == "__main__":
     c = call()
+    fichier = open("config.txt", "r")
+    lines = fichier.readlines()
+    for l in lines:
+        parties = l.split(':')
+        parties[0].replace(' ','')
+        parties[1].replace('\n','')
+        if(parties[0].lower()=="volume"):
+            c.norm = int(parties[1])
+        elif(parties[0].lower()=="touche"):
+            c.key = parties[1].replace('\n','')
+        elif(parties[0].lower()=="temps"):
+            c.t = int(parties[1])
+        else:
+            pass
+            
+
     with sd.Stream(callback=c.print_sound):
         input('Press ENTER to stop...\n')
