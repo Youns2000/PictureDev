e6de652ab7af0fdfb18cc039f71b9511ee56fb7a
Youns2000 <56587811+Youns2000@users.noreply.github.com>
2021-02-16 18:27:24 +0100

    clas

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import numpy as np
 import keyboard
 import tkinter as tk
 import time
+from datetime import datetime as dt
 
-def print_sound(indata, outdata, frames, times, status):
-    volume_norm = np.linalg.norm(indata)*10
-    # print ("|" * int(volume_norm))
-    # all.append(int(volume_norm))
-    if(int(volume_norm)>180):
-        keyboard.press_and_release('alt+tab')
+class call:
+    def __init__(self):
+        self.last = 0
+    def print_sound(self,indata, outdata, frames, times, status):
+        volume_norm = np.linalg.norm(indata)*10
+        if(int(volume_norm)>220 and (int(dt.now().strftime('%S'))-self.last)>1): 
+            self.last = int(dt.now().strftime('%S'))
+            keyboard.press_and_release('space')
 
 if __name__ == "__main__":
-    with sd.Stream(callback=print_sound):
+    c = call()
+    with sd.Stream(callback=c.print_sound):
         input('Press ENTER to stop...\n')
